e8da03b088da796bf06f1dded1e34f4a0bc035d3
Ida Midžić <idmidzic@vsite.hr>
2021-11-30 14:14:08 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Brojevi djeljivi s 3:");
+            for (int idx = 1; idx < 100; ++idx)
+            {
+                if (idx % 3 == 0)
+                {
+                    Console.WriteLine(idx);
+                }
+            }
+            Console.WriteLine("Gotovo!");
+        }
+    }
+}
+
